0d3e1af5bef46252bd4a84e5d1e41ca512cbfedb
FantasyPvP <80643031+FantasyPvP@users.noreply.github.com>
2023-10-04 23:20:36 +0100

    .
    .

--- src/user/bin/rickroll.rs
+++ src/user/bin/rickroll.rs
@@ -5,21 +5,7 @@ use crate::std::application::{
 	Error,
 };
 
-use crate::{println};
-use alloc::{string::String, boxed::Box, vec::Vec};
-
-
-pub struct Rickroll {}
-
-#[async_trait]
-impl Application for Rickroll {
-	fn new() -> Self {
-		Self {}
-	}
-
-	async fn run(&mut self, _args: Vec<String>) -> Result<(), Error> {
-		println!("
-
+const RICKROLL1: &str = "
   _   _                        _____
  | \\ | |                      / ____|
  |  \\| | _____   _____ _ __  | |  __  ___  _ __  _ __   __ _
@@ -33,9 +19,45 @@ impl Application for Rickroll {
  | |__| | |\\ V /  __/    | | (_) | |_| | | |__| | |_) |
   \\_____|_| \\_/ \\___|    |_|\\___/ \\__,_|  \\____/| .__/
                                                 | |
-                                                |_|
+                                                |_|";
+const RICKROLL2: &str = "
+            t0ObOmmQ            ,--.  ,--.
+           {Cdbhkbpwq?          |  ,'.|  | ,---.,--.  ,--.,---. ,--.--.
+           {wzXunrjjCJ          |  |' '  || .-. :\\  `'  /| .-. :|  .--'
+           |mXYzrvrjnv          |  | `   |\\   --. \\    / \\   --.|  |
+            OUCYLrcxjX          `--'  `--' `----'  `--'   `----'`--'
+           LYUUJ0uvjr]           ,----.
+            1CCCZvZx            '  .-./    ,---. ,--,--, ,--,--,  ,--,--.
+             UJJOU1j            |  | .---.| .-. ||      \\|      \\' ,-.  |
+            mXQQQUvC            '  '--'  |' '-' '|  ||  ||  ||  |\\ '-'  |
+        |dooaj0COXcY(hhQ         `------'  `---' `--''--'`--''--' `--`--'
+     tdaoaao*a)UvYz/}khhaaa	     ,----.   ,--.
+   baoooahka**Y1}nX|[hhhahhk    '  .-./   `--',--.  ,--.,---.
+   aoaoohkXhooo|)r1)ahkhaaoo    |  | .---.,--. \\  `'  /| .-. :
+   **##*hLmnnmaJYpvnohhhaaook   '  '--'  ||  |  \\    / \\   --.
+   b#**#00LXvoar11(v#hhhoo*oa    `------' `--'   `--'   `----'
+   b*##MhL0zcaoaxwbhohaa****    ,--.   ,--.                 ,--. ,--.
+  CQ***MJQQcoooaj|uzoooa*##*     \\  `.'  /,---. ,--.,--.    |  | |  | ,---.
+ 0a###w#*mObo*oatXkW*oo#*###p     '.    /| .-. ||  ||  |    |  | |  || .-. |
+ mpZdMMM#p&M***adhM&##*##MMMk       |  | ' '-' ''  ''  '    '  '-'  '| '-' '
+ 1a###w#*mObo*oatXkW*oo#*###p       `--'  `---'  `----'      `-----' |  |-'
+                                                                     `--'";
 
-");
+use crate::{println};
+use alloc::{string::String, boxed::Box, vec::Vec};
+
+
+pub struct Rickroll {}
+
+#[async_trait]
+impl Application for Rickroll {
+	fn new() -> Self {
+		Self {}
+	}
+
+	async fn run(&mut self, _args: Vec<String>) -> Result<(), Error> {
+		println!("{}", RICKROLL2);
 		Ok(())
 	}
 }
+
